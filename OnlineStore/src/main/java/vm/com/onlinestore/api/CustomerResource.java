fb9d3921b692122a9abc0ee666c5099df21a1bd9
vaibhavmathur91 <vaibhav@hiringjunction.com>
2020-05-03 16:10:42 +0530

    Java Learning : 1) Created Customer PATH and DELETE URL

--- OnlineStore/src/main/java/vm/com/onlinestore/api/CustomerResource.java
+++ OnlineStore/src/main/java/vm/com/onlinestore/api/CustomerResource.java
@@ -28,6 +28,20 @@ public class CustomerResource {
 
     @GetMapping(value = "/{customerId}")
     public Customer getCustomer(@PathVariable("customerId") int customerId) {
+        System.out.println("Inside GET localhost:8080/customers/Id ");
         return customerService.getCustomer(customerId);
     }
+
+    @PatchMapping(value = "/{customerId}")
+    public Customer updateCustomer(@PathVariable("customerId") int customerId, @RequestBody Customer customer) {
+        System.out.println("Inside PATCH localhost:8080/customers/Id ");
+        return customerService.updateCustomer(customerId, customer);
+    }
+
+    @DeleteMapping(value = "/{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") int customerId) {
+        System.out.println("Inside DELETE localhost:8080/customers/Id ");
+        customerService.deleteCustomer(customerId);
+    }
+
 }
